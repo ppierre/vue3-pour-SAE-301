a270a4dbb76fba8890df5fdd41f0f412c13dac7c
pierre pracht <pierre.pracht@gmail.com>
2022-08-30 13:34:55 +0200

    ajout heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <h4 class="text-xl">
+      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
       menu (dans <code class="font-mono">/src/App.vue</code>)
     </h4>
     <ul>
@@ -16,3 +17,7 @@
   <!-- Affiche les pages -->
   <router-view class="m-2 border-2 p-2" />
 </template>
+
+<script setup lang="ts">
+import { Bars3Icon } from '@heroicons/vue/20/solid'
+</script>
