b79d43e6c22377c605802ac3ef1344fdddaf5a88
pierre pracht <pierre.pracht@gmail.com>
2022-10-02 13:27:52 +0200

    formkit et formkit/themes/tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
+const formKitTailwind = require("@formkit/themes/tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [formKitTailwind],
+};
